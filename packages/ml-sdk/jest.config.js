cf3e3fe16ca9c2817c6e91e155a8a539c01fa178
Beniasaad Achraf <beni.asaad.achraf@gmail.com>
2020-03-04 15:02:00 +0100

    Add deployement config (#3)
    * add deployement file

--- packages/ml-sdk/jest.config.js
+++ packages/ml-sdk/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
+  reporters: [
+    "default",
+    process.env.CI==='true' ? [ "jest-junit", { outputName: "test-results.xml" } ]: null
+  ].filter(elem => elem!==null)
 }
