ecb4b0ea42ff3f36c1bc4cd5236bdd7df3cf3b49
abeniasaad <achraf.beniasaad@commercetools.de>
2020-03-05 12:42:54 +0100

    config git

--- .circleci/update-ml-sdk.sh
+++ .circleci/update-ml-sdk.sh
@@ -6,13 +6,13 @@ export branch_name="update_spec_changes_$(date +%s)"
 cd $(dirname $0)
 
 
-rm -rf ${SDK_HOME}/src/generated && rm -rf ml-services-api-reference\
+yarn install\
+&&rm -rf ${SDK_HOME}/src/generated && rm -rf ml-services-api-reference\
 && git clone git@github.com:commercetools/ml-services-api-reference.git\
 &&rmf-codegen generate ${ML_API_REF} -o ${SDK_HOME}/src/generated -t typescript_client\
 &&npx tsconfig-replace-paths@0.0.5 --project ${SDK_HOME}/tsconfig-declarations.json -s ${SDK_HOME}/src/generated -o ${SDK_HOME}/src/generated\
 &&find ${SDK_HOME}/src/generated -type f -name "*.ts" | xargs npx organize-imports-cli@0.7.0\
 &&git checkout -b ${branch_name}\
-&&git config --global user.email "$GIT_AUTHOR_EMAIL" && git config --global user.name "$GIT_AUTHOR_NAME"\
 &&git add -u ${SDK_HOME}/src/generated\
 &&git commit -m "new changes coming from api spec"\
 &&git push --set-upstream origin ${branch_name}
