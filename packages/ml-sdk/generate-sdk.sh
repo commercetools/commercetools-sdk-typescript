4993cdd09695a98bdc0da85df85acadc3dfa8617
abeniasaad <achraf.beniasaad@commercetools.de>
2020-03-05 23:41:01 +0100

    enhance scripts

--- packages/ml-sdk/generate-sdk.sh
+++ packages/ml-sdk/generate-sdk.sh
@@ -6,8 +6,4 @@
 rm -rf ./src/generated
 rmf-codegen generate ${ML_API_REF} -o src/generated -t typescript_client
 
-# This command replces path aliases with relative paths, since support for path alias isn't mature enough
-npx tsconfig-replace-paths@0.0.5 --project tsconfig-declarations.json -s ./src/generated -o ./src/generated
-
-# # Optimise imports to make linters happier 
-find src -type f -name "*.ts" | xargs npx organize-imports-cli@0.7.0
+yarn post_process_generate
