5963955a70588e449fc106fd523f4b6fc71c8909
abeniasaad <achraf.beniasaad@commercetools.de>
2020-03-05 14:34:21 +0100

    one last file

--- .circleci/update-ml-sdk.sh
+++ .circleci/update-ml-sdk.sh
@@ -6,8 +6,7 @@ export branch_name="update_spec_changes_$(date +%s)"
 cd $(dirname $0)
 
 
-yarn install&&yarn bootstrap\
-&&rm -rf ${SDK_HOME}/src/generated && rm -rf ml-services-api-reference\
+rm -rf ${SDK_HOME}/src/generated && rm -rf ml-services-api-reference\
 && git clone git@github.com:commercetools/ml-services-api-reference.git\
 &&rmf-codegen generate ${ML_API_REF} -o ${SDK_HOME}/src/generated -t typescript_client\
 &&npx tsconfig-replace-paths@0.0.5 --project ${SDK_HOME}/tsconfig-declarations.json -s ${SDK_HOME}/src/generated -o ${SDK_HOME}/src/generated\
