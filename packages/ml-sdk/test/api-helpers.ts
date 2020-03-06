a224de49e6070b3b0a0a7df1de862d3cbd47e03e
Beniasaad Achraf <beni.asaad.achraf@gmail.com>
2020-03-06 09:17:33 +0100

    add helper (#7)

---
+++ packages/ml-sdk/test/api-helpers.ts
@@ -0,0 +1,49 @@
+import { createAuthMiddlewareForClientCredentialsFlow } from '@commercetools/sdk-middleware-auth'
+import { createHttpMiddleware } from '@commercetools/sdk-middleware-http'
+import { createClient } from '@commercetools/sdk-client'
+
+import { ApiRoot, executeRequest, createExecutorFromMiddlewares } from '../src'
+
+import fetch from 'node-fetch'
+
+const projectKey = requireEnvVar('CTP_PROJECT_KEY')
+const clientId = requireEnvVar('CTP_CLIENT_ID')
+const clientSecret = requireEnvVar('CTP_CLIENT_SECRET')
+const authURL = requireEnvVar('CTP_AUTH_URL')
+const ml_host = requireEnvVar('CTP_ML_API_URL')
+
+const authMiddleware = createAuthMiddlewareForClientCredentialsFlow({
+  host: authURL,
+  projectKey,
+  credentials: {
+    clientId,
+    clientSecret,
+  },
+  fetch,
+})
+
+const httpMiddleware = createHttpMiddleware({
+  host: ml_host,
+  fetch,
+})
+
+const ctpClient = createClient({
+  middlewares: [authMiddleware, httpMiddleware],
+})
+
+const executor: executeRequest = createExecutorFromMiddlewares(
+  ctpClient.execute
+)
+
+export const mlApiBuilder = new ApiRoot({
+  executeRequest: executor,
+  baseUri: ml_host,
+}).withProjectKey({ projectKey })
+
+function requireEnvVar(varName: string): string {
+  const value = process.env[varName]
+  if (value === null || value === undefined) {
+    throw new Error(`environment variable ${varName} not defined`)
+  }
+  return value
+}
