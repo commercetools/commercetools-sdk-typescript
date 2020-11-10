3985e492be85238a37ea71e1a7f5a0e540b4aa00
Michele George <30607195+MicheleRezk@users.noreply.github.com>
2020-11-10 12:59:24 +0100

    add baseUri in createApiBuilderFromCtpClient (#38)
    * fix: add baseUri in createApiBuilderFromCtpClient
    
    * fix: add baseUri in createApiBuilderFromCtpClient

--- packages/platform-sdk/src/ctp/ctp-client.ts
+++ packages/platform-sdk/src/ctp/ctp-client.ts
@@ -1,7 +1,11 @@
 import { ApiRoot } from './../generated/index'
 
-export function createApiBuilderFromCtpClient(ctpClient: any): ApiRoot {
+export function createApiBuilderFromCtpClient(
+  ctpClient: any,
+  baseUri?: string
+): ApiRoot {
   return new ApiRoot({
     executeRequest: ctpClient.execute,
+    baseUri: baseUri,
   })
 }
