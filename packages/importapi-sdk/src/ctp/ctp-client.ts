01c5213d698219c15aa995dd189922ad2c3b3e10
Michele George <michele.george@commercetools.de>
2020-11-10 12:13:16 +0100

    fix: add baseUri in createApiBuilderFromCtpClient

--- packages/importapi-sdk/src/ctp/ctp-client.ts
+++ packages/importapi-sdk/src/ctp/ctp-client.ts
@@ -1,6 +1,9 @@
 import { ApiRoot } from './../generated/index'
 
-export function createApiBuilderFromCtpClient(ctpClient: any): ApiRoot {
+export function createApiBuilderFromCtpClient(
+  ctpClient: any,
+  baseUri?: string
+): ApiRoot {
   return new ApiRoot({
     executeRequest: ctpClient.execute,
   })
