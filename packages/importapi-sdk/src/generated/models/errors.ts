5dc2912d85421a9d0a935501aa8c4e8e7b10dafa
Auto Mation <automation@commercetools.com>
2021-04-27 13:09:19 +0000

    TASK: Updating Platform SDK

--- packages/importapi-sdk/src/generated/models/errors.ts
+++ packages/importapi-sdk/src/generated/models/errors.ts
@@ -52,10 +52,6 @@ export interface ErrorResponse {
    */
   readonly errors?: ErrorObject[]
 }
-/**
- *	An error.
- *
- */
 export type ErrorObject =
   | AccessDeniedError
   | ConcurrentModificationError
@@ -79,10 +75,12 @@ export type ErrorObject =
   | ResourceDeletionError
   | ResourceNotFoundError
   | ResourceUpdateError
+/**
+ *	This is the generic error code for access denied. In case of a wrong scope, an [InvalidScopeError](#invalidscopeerror) will be returned.
+ */
 export interface AccessDeniedError {
   readonly code: 'access_denied'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -94,47 +92,43 @@ export interface AccessDeniedError {
 export interface InvalidScopeError {
   readonly code: 'invalid_scope'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
 }
 /**
- *	The resources involved in the request are not in a valid state for the operation.
- *	The client application should validate the constraints described in the error message before sending the request.
+ *	The resources in the request are not in the valid state for the operation.
+ *	The client application should validate the constraints described in the error message before sending the request again.
  *
  */
 export interface InvalidOperation {
   readonly code: 'InvalidOperation'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
 }
 /**
- *	The Unique AttributeConstraint was violated.
+ *	The `Unique` [Attribute Constraint](/../api/projects/productTypes#attributeconstraint-enum) was violated.
  */
 export interface DuplicateAttributeValueError {
   readonly code: 'DuplicateAttributeValue'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
   /**
-   *	The conflicting attribute.
+   *	The attribute in conflict.
    *
    */
   readonly attribute: Attribute
 }
 /**
- *	The CombinationUnique AttributeConstraint was violated.
+ *	The `CombinationUnique` [Attribute Constraint](/../api/projects/productTypes#attributeconstraint-enum) was violated.
  */
 export interface DuplicateAttributeValuesError {
   readonly code: 'DuplicateAttributeValues'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -144,12 +138,11 @@ export interface DuplicateAttributeValuesError {
   readonly attributes: Attribute[]
 }
 /**
- *	A value for a field conflicts with an existing duplicate value.
+ *	The given value already exists for a field that is checked for unique values.
  */
 export interface DuplicateFieldError {
   readonly code: 'DuplicateField'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -165,14 +158,13 @@ export interface DuplicateFieldError {
   readonly duplicateValue?: any
 }
 /**
- *	A given combination of variant values conflicts with an existing one.
- *	Every product variant must have a distinct combination of SKU, prices, and custom attribute values.
+ *	The given combination of values of a [Product Variant](/../api/projects/products#productvariant) conflicts with an existing one.
+ *	Every [Product Variant](/../api/projects/products#productvariant) must have a distinct combination of SKU, prices, and custom attribute values.
  *
  */
 export interface DuplicateVariantValuesError {
   readonly code: 'DuplicateVariantValues'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -199,7 +191,6 @@ export interface VariantValues {
 export interface InsufficientScopeError {
   readonly code: 'insufficient_scope'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -207,7 +198,6 @@ export interface InsufficientScopeError {
 export interface InvalidCredentialsError {
   readonly code: 'InvalidCredentials'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -215,18 +205,18 @@ export interface InvalidCredentialsError {
 export interface InvalidTokenError {
   readonly code: 'invalid_token'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
 }
 /**
- *	A field has an invalid value.
+ *	A given field is not supported.
+ *	This error occurs, for example, if the field `variants`, which is not supported by [Product Import](/product#productimport), is sent to the Product Import endpoint.
+ *
  */
 export interface InvalidFieldError {
   readonly code: 'InvalidField'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -241,34 +231,32 @@ export interface InvalidFieldError {
    */
   readonly invalidValue: any
   /**
-   *	A fixed set of allowed values for the field, if any.
+   *	The set of allowed values for the field, if any.
    *
    */
   readonly allowedValues?: any[]
 }
 /**
- *	Invalid JSON input has been sent to the service. Either the JSON is syntactically not correct, or the JSON does not
- *	conform to the expected shape (e.g. is missing a required field). The client application should validate the input
- *	according to the constraints described in the error message before sending the request.
+ *	An invalid JSON input has been sent to the service.
+ *	Either the JSON is syntactically incorrect or the JSON has an unexpected shape, for example, a required field is missing.
+ *	The client application should validate the input according to the constraints described in the error message before sending the request again.
  *
  */
 export interface InvalidJsonInput {
   readonly code: 'InvalidJsonInput'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
 }
 /**
- *	Invalid input has been sent to the service. The client application should validate the input according to the
- *	constraints described in the error message before sending the request.
+ *	An invalid input has been sent to the service. The client application should validate the input according to the
+ *	constraints described in the error message before sending the request again.
  *
  */
 export interface InvalidInput {
   readonly code: 'InvalidInput'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -276,7 +264,6 @@ export interface InvalidInput {
 export interface ResourceNotFoundError {
   readonly code: 'ResourceNotFound'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -288,7 +275,6 @@ export interface ResourceNotFoundError {
 export interface ResourceCreationError {
   readonly code: 'ResourceCreation'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -300,7 +286,6 @@ export interface ResourceCreationError {
 export interface ResourceUpdateError {
   readonly code: 'ResourceUpdate'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -312,7 +297,6 @@ export interface ResourceUpdateError {
 export interface ResourceDeletionError {
   readonly code: 'ResourceDeletion'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -327,7 +311,6 @@ export interface ResourceDeletionError {
 export interface RequiredFieldError {
   readonly code: 'RequiredField'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -340,7 +323,6 @@ export interface RequiredFieldError {
 export interface InvalidStateTransitionError {
   readonly code: 'InvalidTransition'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -358,15 +340,14 @@ export interface InvalidStateTransitionError {
   readonly newState: ProcessingState
 }
 /**
- *	The request conflicts with the current state of the involved resource(s). Typically, the request attempts to modify a resource
- *	that is out of date, i.e. that has been modified by another client since the last time it was retrieved.
- *	The client application should resolve the conflict (with or without involving the end-user) before retrying the request
+ *	The request conflicts with the current state of the involved resources.
+ *	This error typically occurs when the request attempts to modify a resource that is out of date, that is, it has been modified by another client since the last time it was retrieved by the system attempting to update it.
+ *	The client application should resolve the conflict (with or without involving the end-user) before retrying the request.
  *
  */
 export interface ConcurrentModificationError {
   readonly code: 'ConcurrentModification'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -381,7 +362,7 @@ export interface ConcurrentModificationError {
    */
   readonly currentVersion: number
   /**
-   *	The conflicted resource.
+   *	The resource in conflict.
    *
    */
   readonly conflictedResource?: any
@@ -389,7 +370,6 @@ export interface ConcurrentModificationError {
 export interface ContentionError {
   readonly code: 'Contention'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
@@ -397,7 +377,6 @@ export interface ContentionError {
 export interface GenericError {
   readonly code: 'Generic'
   /**
-   *	This is a placeholder for an actual platform error message.
    *
    */
   readonly message: string
