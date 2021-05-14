f4d19ca1cc271f1e6061763cef007637368ce400
Auto Mation <automation@commercetools.com>
2021-05-14 10:21:41 +0000

    TASK: Updating Platform SDK

--- packages/importapi-sdk/src/generated/models/products.ts
+++ packages/importapi-sdk/src/generated/models/products.ts
@@ -71,7 +71,7 @@ export interface WhitespaceTokenizer {
   readonly type: 'whitespace'
 }
 /**
- *	Import representation for a prduct.
+ *	Import representation for a product.
  *
  *	The import representation for a product is the most minimal representation required
  *	for creating a product in commercetools.
