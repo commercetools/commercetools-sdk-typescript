905f9f06b872937254ac1618cd2d54295577da8f
Chukwuemeka Ajima <meeky.ae@gmail.com>
2021-06-03 03:46:36 +0200

    feat(sdk-client): build the typescript sdk-client

---
+++ packages/sdk-client/test/sdk-client.test.ts
@@ -0,0 +1,496 @@
+import qs from 'querystring'
+import { createClient } from '../src'
+import { ClientRequest } from '../types/sdk'
+
+const createPayloadResult = (tot, startingId = 0) => ({
+  count: tot,
+  results: Array.from(Array(tot), (_, index) => ({
+    id: String(index + 1 + startingId),
+  })),
+})
+
+describe('validate options', () => {
+  test('middlewares is required', () => {
+    expect(() => createClient(null)).toThrow('Missing required options')
+  })
+
+  test('middlewares must be an array', () => {
+    expect(() => createClient({ middlewares: {} as any })).toThrow(
+      'Middlewares should be an array'
+    )
+  })
+
+  test('middlewares must not be an empty array', () => {
+    expect(() => createClient({ middlewares: [] })).toThrow(
+      'You need to provide at least one middleware'
+    )
+  })
+})
+
+describe('api', () => {
+  const middlewares = [next => (...args) => next(...args)]
+  const client = createClient({ middlewares })
+  const request: ClientRequest = {
+    uri: '/foo',
+    method: 'POST',
+  }
+
+  test('expose "execute" function', () => {
+    expect(typeof client.execute).toBe('function')
+  })
+
+  test('execute should return a promise', () => {
+    const promise = client.execute(request)
+    expect(promise.then).toBeDefined()
+  })
+})
+
+describe('execute', () => {
+  const request = {
+    uri: '/test/products',
+    method: 'GET',
+    body: null,
+    headers: {},
+  } as any
+
+  test('should throw if request is missing', () => {
+    const middlewares = [next => (...args) => next(...args)]
+    const client = createClient({ middlewares })
+    expect(() => client.execute(null)).toThrow(
+      /The "exec" function requires a "Request" object/
+    )
+  })
+
+  test('should throw if request uri is invalid', () => {
+    const middlewares = [next => (...args) => next(...args)]
+    const client = createClient({ middlewares })
+    const badRequest = {
+      ...request,
+      uri: 24,
+    } as any
+    expect(() => client.execute(badRequest)).toThrow(
+      /The "exec" Request object requires a valid uri/
+    )
+  })
+
+  test('should throw if request method is invalid', () => {
+    const middlewares = [next => (...args) => next(...args)]
+    const client = createClient({ middlewares })
+    const badRequest = {
+      ...request,
+      method: 'INVALID_METHOD',
+    } as any
+    expect(() => client.execute(badRequest)).toThrow(
+      /The "exec" Request object requires a valid method./
+    )
+  })
+
+  test('execute and resolve a simple request', () => {
+    const client = createClient({
+      middlewares: [
+        next => (req, res) => {
+          const headers = {
+            Authorization: 'Bearer 123',
+          }
+          next({ ...req, headers }, res)
+        },
+        next => (req, res) => {
+          const body = {
+            id: '123',
+            version: 1,
+          }
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    })
+
+    return client.execute(request).then(response => {
+      expect(response).toEqual({
+        body: {
+          id: '123',
+          version: 1,
+        },
+        statusCode: 200,
+      })
+    })
+  })
+
+  test('execute and reject a request', () => {
+    const client = createClient({
+      middlewares: [
+        next => (req, res) => {
+          const error = new Error('Invalid password') as any
+          next(req, { ...res, error, statusCode: 400 })
+        },
+      ],
+    })
+
+    return client
+      .execute(request)
+      .then(() =>
+        Promise.reject(
+          new Error(
+            'This function should never be called, the response was rejected'
+          )
+        )
+      )
+      .catch(error => {
+        expect(error.message).toEqual('Invalid password')
+        return Promise.resolve()
+      })
+  })
+
+  describe('ensure correct functions are used to resolve the promise', () => {
+    test('resolve', () => {
+      const customResolveSpy = jest.fn()
+      const client = createClient({
+        middlewares: [
+          next => (req, res) => {
+            const responseWithCustomResolver = {
+              resolve() {
+                customResolveSpy()
+                res.resolve(null)
+              },
+            }
+            next(req, responseWithCustomResolver)
+          },
+        ],
+      })
+
+      return client.execute(request).then(() => {
+        expect(customResolveSpy).toHaveBeenCalled()
+      })
+    })
+
+    test('reject', () => {
+      const customRejectSpy = jest.fn()
+      const client = createClient({
+        middlewares: [
+          next => (req, res) => {
+            const responseWithCustomResolver = {
+              reject() {
+                customRejectSpy()
+                res.reject()
+              },
+              error: new Error('Oops'),
+            }
+            next(req, responseWithCustomResolver)
+          },
+        ],
+      })
+
+      return client.execute(request).catch(() => {
+        expect(customRejectSpy).toHaveBeenCalled()
+      })
+    })
+  })
+})
+
+describe('process', () => {
+  const request = {
+    uri: '/test/products',
+    method: 'GET',
+    body: null,
+    headers: {},
+  }
+
+  describe('validate arguments', () => {
+    const middlewares = [next => (...args) => next(...args)]
+    const client = createClient({ middlewares })
+
+    test('should throw if second argument missing', () => {
+      expect(() => client.process(request)).toThrow(
+        /The "process" function accepts a "Function"/
+      )
+    })
+
+    test('should throw if second argument is not a function', () => {
+      expect(() => client.process(request, 'foo')).toThrow(
+        /The "process" function accepts a "Function"/
+      )
+    })
+
+    test('should throw if request method is not `GET`', () => {
+      expect(() =>
+        client.process({ uri: 'foo', method: 'POST' }, () => {})
+      ).toThrow(/The "process" Request object requires a valid method/)
+    })
+  })
+
+  test('process and resolve paginating 3 times', () => {
+    let reqCount = 0
+    const reqStubs = {
+      0: {
+        body: createPayloadResult(20),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          limit: '20',
+        },
+      },
+      1: {
+        body: createPayloadResult(20),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          where: 'id > "20"',
+          limit: '20',
+        },
+      },
+      2: {
+        body: createPayloadResult(10),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          where: 'id > "20"',
+          limit: '20',
+        },
+      },
+    }
+
+    const client = createClient({
+      middlewares: [
+        next => (req, res) => {
+          const body = reqStubs[reqCount].body
+          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+            reqStubs[reqCount].query
+          )
+
+          reqCount += 1
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    })
+
+    return client
+      .process(request, () => Promise.resolve('OK'))
+      .then(response => {
+        expect(response).toEqual(['OK', 'OK', 'OK'])
+      })
+  })
+
+  test('return only the required number of items', () => {
+    let reqCount = 0
+    const reqStubs = {
+      0: {
+        body: createPayloadResult(20),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          limit: '20',
+        },
+      },
+      1: {
+        body: createPayloadResult(20),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          where: 'id > "20"',
+          limit: '20',
+        },
+      },
+      2: {
+        body: createPayloadResult(6),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          where: 'id > "20"',
+          limit: '6',
+        },
+      },
+    }
+
+    const client = createClient({
+      middlewares: [
+        next => (req, res) => {
+          const body = reqStubs[reqCount].body
+          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+            reqStubs[reqCount].query
+          )
+
+          reqCount += 1
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    })
+
+    return client
+      .process(request, () => Promise.resolve('OK'), { total: 46 })
+      .then(response => {
+        expect(response).toEqual(['OK', 'OK', 'OK'])
+      })
+  })
+
+  test('process and resolve pagination by preserving original query', () => {
+    let reqCount = 0
+    const reqStubs = {
+      0: {
+        body: createPayloadResult(5),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: 'name (en = "Foo")',
+          limit: '5',
+        },
+      },
+      1: {
+        body: createPayloadResult(2),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: ['id > "5"', 'name (en = "Foo")'],
+          limit: '5',
+        },
+      },
+    }
+
+    const client = createClient({
+      middlewares: [
+        next => (req, res) => {
+          const body = reqStubs[reqCount].body
+          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+            reqStubs[reqCount].query
+          )
+
+          reqCount += 1
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    })
+
+    // @ts-ignore
+    return client.process(
+      {
+        ...request,
+        uri: `${request.uri}?${qs.stringify({
+          sort: 'createdAt desc',
+          where: 'name (en = "Foo")',
+          limit: 5,
+        })}`,
+      },
+      () => Promise.resolve('OK')
+    )
+  })
+
+  test('process should not call fn when last page is empty', async () => {
+    let reqCount = 0
+    const reqStubs = {
+      0: {
+        body: createPayloadResult(5),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: 'name (en = "Foo")',
+          limit: '5',
+        },
+      },
+      1: {
+        body: createPayloadResult(5, 5),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: ['id > "5"', 'name (en = "Foo")'],
+          limit: '5',
+        },
+      },
+      2: {
+        body: createPayloadResult(0),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: ['id > "10"', 'name (en = "Foo")'],
+          limit: '5',
+        },
+      },
+    }
+
+    const client = createClient({
+      middlewares: [
+        next => (req, res) => {
+          const body = reqStubs[reqCount].body
+          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+            reqStubs[reqCount].query
+          )
+
+          reqCount += 1
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    })
+
+    let fnCall = 0
+    // @ts-ignore
+    const processRes = await client.process(
+      {
+        ...request,
+        uri: `${request.uri}?${qs.stringify({
+          sort: 'createdAt desc',
+          where: 'name (en = "Foo")',
+          limit: 5,
+        })}`,
+      },
+      res => {
+        expect(res.body.results).toEqual(reqStubs[fnCall].body.results)
+        expect(fnCall).toBeLessThan(2) // should not call fn if the last page is empty
+
+        fnCall += 1
+
+        return `OK${fnCall}`
+      },
+      {
+        accumulate: true,
+      }
+    )
+
+    expect(processRes).toEqual(['OK1', 'OK2']) // results from fn calls
+    expect(fnCall).toBe(2) // fn was called two times
+  })
+
+  test('process and reject a request', () => {
+    const client = createClient({
+      middlewares: [
+        next => (req, res) => {
+          const error = new Error('Invalid password')
+          next(req, { ...res, error, statusCode: 400 })
+        },
+      ],
+    })
+
+    return client
+      .process(request, () => Promise.resolve('OK'))
+      .then(() =>
+        Promise.reject(
+          new Error(
+            'This function should never be called, the response was rejected'
+          )
+        )
+      )
+      .catch(error => {
+        expect(error.message).toEqual('Invalid password')
+        return Promise.resolve()
+      })
+  })
+
+  test('process and reject on rejection from user', () => {
+    const client = createClient({
+      middlewares: [
+        next => (req, res) => {
+          next(req, { ...res, statusCode: 200 })
+        },
+      ],
+    })
+
+    return client
+      .process(request, () => Promise.reject(new Error('Rejection from user')))
+      .then(() =>
+        Promise.reject(
+          new Error(
+            'This function should never be called, the response was rejected'
+          )
+        )
+      )
+      .catch(error => {
+        expect(error).toEqual(new Error('Rejection from user'))
+      })
+  })
+})
---
+++ packages/sdk-client/types/sdk.d.ts
@@ -0,0 +1,467 @@
+export type JsonObject<T = unknown> = { [key: string]: T }
+
+export type MethodType =
+  | 'ACL'
+  | 'BIND'
+  | 'CHECKOUT'
+  | 'CONNECT'
+  | 'COPY'
+  | 'DELETE'
+  | 'GET'
+  | 'HEAD'
+  | 'LINK'
+  | 'LOCK'
+  | 'M-SEARCH'
+  | 'MERGE'
+  | 'MKACTIVITY'
+  | 'MKCALENDAR'
+  | 'MKCOL'
+  | 'MOVE'
+  | 'NOTIFY'
+  | 'OPTIONS'
+  | 'PATCH'
+  | 'POST'
+  | 'PROPFIND'
+  | 'PROPPATCH'
+  | 'PURGE'
+  | 'PUT'
+  | 'REBIND'
+  | 'REPORT'
+  | 'SEARCH'
+  | 'SOURCE'
+  | 'SUBSCRIBE'
+  | 'TRACE'
+  | 'UNBIND'
+  | 'UNLINK'
+  | 'UNLOCK'
+  | 'UNSUBSCRIBE'
+
+export type HttpErrorType = {
+  name: string
+  message: string
+  code: number
+  status: number
+  statusCode: number
+  originalRequest: ClientRequest
+  body?: JsonObject
+  headers?: JsonObject<string>
+}
+
+/* Middlewares */
+export type MiddlewareRequest = ClientRequest
+
+export type MiddlewareResponse = {
+  resolve(response: JsonObject): void
+  reject(error: JsonObject): void
+  body?: JsonObject
+  error?: HttpErrorType
+  statusCode: number
+  headers?: JsonObject<string>
+  request?: JsonObject
+}
+
+export type Dispatch = (
+  request: MiddlewareRequest,
+  response: MiddlewareResponse
+) => unknown
+
+export type Middleware = (next: Dispatch) => Dispatch
+
+/* Client */
+export type ClientRequest = {
+  uri: string
+  method: MethodType
+  body?: string | JsonObject
+  headers?: JsonObject<string>
+}
+export type AuthRequest = {
+  uri: string
+  body: string
+  basicAuth: string
+  authType: string
+  headers?: JsonObject<string>
+}
+
+export type ClientResponse = {
+  body?: Object
+  error?: HttpErrorType
+  statusCode: number
+  headers?: JsonObject<string>
+  request?: Object
+}
+
+export type SuccessResult = {
+  body: {
+    results: JsonObject<JsonObject<string>>
+    count: number
+  }
+  statusCode: number
+  headers?: JsonObject<string>
+}
+
+export type ClientResult = SuccessResult | HttpErrorType
+
+export type ProcessFn = (result: SuccessResult) => Promise<any>
+
+export type Client = {
+  execute: (request: ClientRequest) => Promise<any>
+  process: (request: ClientRequest, fn: ProcessFn, Object) => Promise<any>
+}
+
+export type ProcessOptions = {
+  accumulate?: boolean
+  total?: number
+}
+
+export type ValiadateOption = {
+  allowedMethods: MethodType
+}
+
+export type ClientOptions = {
+  middlewares: Array<Middleware>
+}
+
+export type AuthMiddlewareOptions = {
+  host: string
+  projectKey: string
+  credentials: {
+    clientId: string
+    clientSecret: string
+    anonymousId?: string
+  }
+  scopes: Array<string>
+  // For internal usage only
+  oauthUri: string
+  fetch?: typeof fetch
+  tokenCache?: TokenCache
+}
+
+export type AuthOptions = {
+  host: string
+  token?: string
+  authType?: string
+  projectKey?: string
+  disableRefreshToken?: boolean
+  credentials: {
+    clientId: string
+    clientSecret: string
+  }
+  headers?: JsonObject<string>
+  scopes?: Array<string>
+  fetch?: typeof fetch
+}
+
+export type CustomAuthOptions = {
+  host?: string
+  token?: string
+  authType?: string
+  projectKey?: string
+  disableRefreshToken?: boolean
+  credentials?: {
+    clientId: string
+    clientSecret: string
+  }
+  headers?: JsonObject<string>
+  scopes?: Array<string>
+  fetch?: typeof fetch
+}
+
+export type TokenInfo = {
+  refresh_token: string
+  access_token: string
+  expires_at: number
+  expires_in?: number
+  scope?: string
+  token_type?: string
+}
+
+export type RefreshAuthMiddlewareOptions = {
+  host: string
+  projectKey: string
+  credentials: {
+    clientId: string
+    clientSecret: string
+  }
+  refreshToken: string
+  // For internal usage only
+  oauthUri: string
+  fetch?: typeof fetch
+}
+
+export type Task = {
+  request: MiddlewareRequest
+  response: MiddlewareResponse
+  next: Next
+}
+
+export type RequestState = boolean
+export type TokenStore = {
+  token: string
+  expirationTime: number
+  refreshToken?: string
+}
+
+export type TokenCacheOptions = {
+  clientId: string
+  projectKey: string
+  host: string
+}
+
+export type TokenCache = {
+  get: (tokenCacheOptions?: TokenCacheOptions) => TokenStore
+  set: (cache: TokenStore, tokenCacheOptions?: TokenCacheOptions) => void
+}
+
+/* Request */
+type requestBaseOptions = {
+  response: MiddlewareResponse
+  url: string
+  body: string
+  basicAuth: string
+  pendingTasks: Array<Task>
+  requestState: {
+    get: () => RequestState
+    set: (requestState: RequestState) => void
+  }
+  tokenCache: TokenCache
+  tokenCacheKey?: TokenCacheOptions
+}
+export type executeRequestOptions = requestBaseOptions & {
+  fetcher: typeof fetch
+}
+
+export type AuthMiddlewareBaseOptions = requestBaseOptions & {
+  request: MiddlewareRequest
+  fetch?: typeof fetch
+}
+
+export type UserAuthOptions = {
+  username: string
+  password: string
+}
+
+export type ClientAuthOptions = {
+  clientId: string
+  clientSecret: string
+}
+
+export type PasswordAuthMiddlewareOptions = {
+  host: string
+  projectKey: string
+  credentials: {
+    clientId: string
+    clientSecret: string
+    user: UserAuthOptions
+  }
+  scopes: Array<string>
+  // For internal usage only
+  oauthUri: string
+  fetch?: typeof fetch
+}
+
+export type HttpMiddlewareOptions = {
+  host: string
+  credentialsMode?: 'omit' | 'same-origin' | 'include'
+  includeHeaders?: boolean
+  includeResponseHeaders?: boolean
+  includeOriginalRequest?: boolean
+  maskSensitiveHeaderData?: boolean
+  timeout?: number
+  enableRetry?: boolean
+  retryConfig?: {
+    maxRetries?: number
+    retryDelay?: number
+    backoff?: boolean
+    maxDelay?: number
+  }
+  fetch?: typeof fetch
+  abortController?: AbortController // deprecated
+  getAbortController: () => AbortController
+}
+export type QueueMiddlewareOptions = {
+  concurrency: number
+}
+export type UserAgentMiddlewareOptions = {
+  libraryName?: string
+  libraryVersion?: string
+  contactUrl?: string
+  contactEmail?: string
+}
+
+export type Next = (
+  request: MiddlewareRequest,
+  response: MiddlewareResponse
+) => mixed
+
+/* API Request Builder */
+export type ServiceBuilderDefaultParams = {
+  expand: Array<string>
+  searchKeywords: Array<{ lang: string; value: string }>
+  pagination: {
+    page: number | null
+    perPage: number | null
+    sort: Array<string>
+    withTotal: boolean | null
+  }
+  id?: string | null
+  staged?: boolean
+  priceCurrency?: string
+  priceCountry?: string
+  priceCustomerGroup?: string
+  priceChannel?: string
+  query?: {
+    operator: 'and' | 'or'
+    where: Array<string>
+  }
+  location?: {
+    country?: string
+    currency?: string
+    state?: string
+  }
+  search?: {
+    facet: Array<string>
+    filter: Array<string>
+    filterByQuery: Array<string>
+    filterByFacets: Array<string>
+    fuzzy: boolean
+    fuzzyLevel: number
+    markMatchingVariants: boolean
+    text: {
+      lang: string
+      value: string
+    } | null
+  }
+  version?: number
+  customerId?: string
+  cartId?: string
+  dataErasure?: string
+  applyOrderEditTo?: string
+}
+export type ServiceBuilderParams = {
+  // query-expand
+  expand?: Array<string>
+
+  // query-id
+  id?: string | null
+  key?: string | null
+  customerId?: string | null
+  cartId?: string | null
+
+  // query-page
+  sort: Array<{ by: string; direction: 'asc' | 'desc' }>
+  page: number | null
+  perPage: number | null
+  withTotal: boolean | null
+
+  // query-projection
+  staged?: boolean
+  priceCurrency?: string
+  priceCountry?: string
+  priceCustomerGroup?: string
+  priceChannel?: string
+
+  // query-search
+  text?: {
+    language?: string
+    value?: string
+  } | null
+  fuzzy?: boolean
+  fuzzyLevel?: number
+  markMatchingVariants?: boolean
+  facet?: Array<string>
+  filter?: Array<string>
+  filterByQuery?: Array<string>
+  filterByFacets?: Array<string>
+
+  // query-suggest
+  searchKeywords?: Array<{ language: string; value: string }>
+
+  // query
+  where?: Array<string>
+  whereOperator?: 'and' | 'or'
+
+  // query-location
+  country?: string
+  currency?: string
+  state?: string
+
+  // version
+  version?: string
+
+  // data-erasure
+  dataErasure?: string
+
+  // applyOrderEditTo
+  applyOrderEditTo?: boolean
+
+  // container
+  container?: string | null
+
+  // params
+  orderNumber?: number
+}
+export type ServiceBuilder = {
+  type: string
+  features: Array<string>
+  params: ServiceBuilderDefaultParams
+  build(): string
+}
+export type ServiceBuilderDefinition = {
+  type: string
+  endpoint: string
+  features: Array<string>
+}
+export type ServiceBuilderInstance = {
+  withVersion: (version: number) => Object
+  withFullDataErasure(): Object
+  where: (predicate: string) => Object
+  whereOperator: (option: string) => Object
+  sort: (option: string) => Object
+  page: (page: number) => Object
+  perPage: (amount: number) => Object
+  withTotal(value: boolean): Object
+  byId: (id: string) => Object
+  byKey: (key: string) => Object
+  byCustomerId: (id: string) => Object
+  byCartId: (id: string) => Object
+  expand: (string: string) => Object
+  build(): string
+  parse(): string
+}
+export type ApiRequestBuilder = {
+  [key: string]: ServiceBuilder
+}
+
+/* HTTP User Agent */
+export type HttpUserAgentOptions = {
+  name: string
+  version?: string
+  libraryName?: string
+  libraryVersion?: string
+  contactUrl?: string
+  contactEmail?: string
+}
+
+/* Sync Actions */
+export type UpdateAction = {
+  action: string
+  [key: string]: any
+}
+export type SyncAction = {
+  buildActions: (now: Object, before: Object) => Array<UpdateAction>
+}
+export type SyncActionConfig = {
+  shouldOmitEmptyString: boolean
+}
+export type ActionGroup = {
+  type: string
+  group: 'ignore' | 'allow'
+}
+
+export type ExistingTokenMiddlewareOptions = {
+  force?: boolean
+}
+
+export type CorrelationIdMiddlewareOptions = {
+  generate: () => string
+}
