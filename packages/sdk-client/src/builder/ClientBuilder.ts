2a5d8c92923fefc8df7074e71bf0008f9418f212
Chukwuemeka Ajima <meeky.ae@gmail.com>
2021-07-29 10:59:56 +0200

    chore(client-builder): remove the private identifier from the withMiddleware method.

--- packages/sdk-client/src/builder/ClientBuilder.ts
+++ packages/sdk-client/src/builder/ClientBuilder.ts
@@ -64,7 +64,7 @@ export default class ClientBuilder {
       .withLoggerMiddleware()
   }
 
-  private withAuthMiddleware(authMiddleware: Middleware): ClientBuilder {
+  withAuthMiddleware(authMiddleware: Middleware): ClientBuilder {
     this.authMiddleware = authMiddleware
     return this
   }
