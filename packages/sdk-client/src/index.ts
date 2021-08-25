d23d574f512e91a41a48a5c3ba3fae3d6685009b
Ajima Chukwuemeka <32770340+ajimae@users.noreply.github.com>
2021-08-25 07:41:30 +0200

    chore(sdk-client-v2): final package cleanup and refactor (#104)

--- packages/sdk-client/src/index.ts
+++ packages/sdk-client/src/index.ts
@@ -1,17 +1,18 @@
-// clients
 export { default as createClient } from './sdk-client/client'
 export { default as getErrorByCode } from './sdk-client/errors'
 export * as errors from './sdk-client/errors'
+export * from './types/sdk.d'
 
-// auth
 export { default as createAuthForClientCredentialsFlow } from './sdk-middleware-auth/client-credentials-flow'
 export { default as createAuthForPasswordFlow } from './sdk-middleware-auth/password-flow'
 export { default as createAuthForRefreshTokenFlow } from './sdk-middleware-auth/refresh-token-flow'
 export { default as createAuthForAnonymousSessionFlow } from './sdk-middleware-auth/anonymous-session-flow'
 export { default as createAuthWithExistingToken } from './sdk-middleware-auth/existing-token'
-// http
+export { default as createCorrelationIdMiddleware } from './sdk-middleware-correlation-id/correlation-id'
+export { default as createLoggerMiddleware } from './sdk-middleware-logger/logger'
+export { default as createUserAgentMiddleware } from './sdk-middleware-user-agent/user-agent'
+export { default as createQueueMiddleware } from './sdk-middleware-queue/queue'
 export { default as createHttpClient } from './sdk-middleware-http/http'
 
 export { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk'
-export { default as ApiRootBuilder } from './builder/ClientBuilder';
 export { default as ClientBuilder} from './builder/ClientBuilder';
