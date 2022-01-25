5cabe67a20f839926d2f26325e099410b9019898
Ajima Chukwuemeka <32770340+ajimae@users.noreply.github.com>
2022-01-25 19:09:29 +0100

    chore(examples): add react example (#176)
    - add example to show how the TS SDK is used in a react project

---
+++ examples/react/webpack.config.js
@@ -0,0 +1,75 @@
+const webpack = require('webpack')
+const Dotenv = require('dotenv-webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
+const generalConfig = {
+  watchOptions: {
+    aggregateTimeout: 600,
+    ignored: /node_modules/,
+  },
+  entry: './index.js',
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+      protectWebpackAssets: false,
+      cleanOnceBeforeBuildPatterns: ['dist'],
+      cleanAfterEveryBuildPatterns: ['*.LICENSE.txt'],
+    }),
+    new NodePolyfillPlugin(),
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+    new Dotenv({
+      path: './.env',
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    fallback: {
+      path: require.resolve('path-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      'crypto-browserify': require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer/'),
+    },
+  },
+}
+
+module.exports = () => {
+  return [generalConfig]
+}
