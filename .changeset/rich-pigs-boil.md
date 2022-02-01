b5ff8eccad7ff12ecaa7c592aa94e0706cdc847a
Ajima Chukwuemeka <32770340+ajimae@users.noreply.github.com>
2022-02-01 23:17:45 +0100

    Add Vue3 Example (#184)
    * chore(vue3): add vue demo
    
    - add vue3 basic demp app
    
    * chore(env): add .env.exmple file
    
    * Create rich-pigs-boil.md
    
    Co-authored-by: Jens Schulze <jens.schulze@commercetools.com>

---
+++ .changeset/rich-pigs-boil.md
@@ -0,0 +1,5 @@
+---
+
+---
+
+Add Vue3 Example
---
+++ examples/vue/src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button type="button" :style="{background: color}" class="btn" @click="onClick()">{{text}}</button>
+</template>
+
+<script>
+
+export default ({
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+    onClick: Function,
+  }
+})
+
+</script>
