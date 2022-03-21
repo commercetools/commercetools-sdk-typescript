4859b5c59270ca45cfa8ed96970e1481e6527320
Ajima Chukwuemeka <32770340+ajimae@users.noreply.github.com>
2022-03-21 16:33:48 +0100

    chore(sdk-client): expose process function (#220)
    - refactor the client to expose the process function at the client level
    - write tests to ensure new and existing features still works properly

--- packages/sdk-client/test/client.test/sdk-client.test.ts
+++ packages/sdk-client/test/client.test/sdk-client.test.ts
@@ -1,10 +1,12 @@
 import qs from 'querystring'
-import { createClient } from '../../src/sdk-client'
+import { Process, createClient, ClientBuilder } from '../../src'
+import { createApiBuilderFromCtpClient } from '../../../platform-sdk'
 import {
   ClientRequest,
   HttpErrorType,
   MiddlewareResponse,
 } from '../../src/types/sdk.d'
+import fetch from 'node-fetch'
 
 const createPayloadResult = (tot, startingId = 0) => ({
   count: tot,
@@ -521,5 +523,374 @@ describe('process', () => {
       .catch((error) => {
         expect(error).toEqual(new Error('Rejection from user'))
       })
+  })
+})
+
+// exposed process function
+describe('process - exposed', () => {
+  const request = {
+    uri: '/test/products',
+    method: 'GET',
+    body: null,
+    headers: {},
+  } as any
+
+  describe('validate arguments', () => {
+    const middlewares = [
+      (next) =>
+        (...args) =>
+          next(...args),
+    ]
+
+    createClient({ middlewares }) as any
+    test('should throw if second argument missing', () => {
+      expect(() => Process(request, null, {})).toThrow(
+        /The "process" function accepts a "Function"/
+      )
+    })
+
+    test('should throw if second argument is not a function', () => {
+      expect(() => Process(request, undefined, {})).toThrow(
+        /The "process" function accepts a "Function"/
+      )
+    })
+
+    test('should throw if request method is not `GET`', () => {
+      expect(() =>
+        Process(
+          { uri: 'foo', method: 'POST' },
+          (res) => Promise.resolve(res),
+          {}
+        )
+      ).toThrow(/The "process" Request object requires a valid method/)
+    })
+  })
+
+  test('process and resolve paginating 3 times', () => {
+    let reqCount = 0
+    const reqStubs = {
+      0: {
+        body: createPayloadResult(20),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          limit: '20',
+        },
+      },
+      1: {
+        body: createPayloadResult(20),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          where: 'id > "20"',
+          limit: '20',
+        },
+      },
+      2: {
+        body: createPayloadResult(10),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          where: 'id > "20"',
+          limit: '20',
+        },
+      },
+    }
+
+    createClient({
+      middlewares: [
+        (next) => (req, res) => {
+          const body = reqStubs[reqCount].body
+          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+            reqStubs[reqCount].query
+          )
+
+          reqCount += 1
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    }) as any
+
+    return Process(request, () => Promise.resolve('OK'), {}).then(
+      (response) => {
+        expect(response).toEqual(['OK', 'OK', 'OK'])
+      }
+    )
+  })
+
+  test('return only the required number of items', () => {
+    let reqCount = 0
+    const reqStubs = {
+      0: {
+        body: createPayloadResult(20),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          limit: '20',
+        },
+      },
+      1: {
+        body: createPayloadResult(20),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          where: 'id > "20"',
+          limit: '20',
+        },
+      },
+      2: {
+        body: createPayloadResult(6),
+        query: {
+          sort: 'id asc',
+          withTotal: 'false',
+          where: 'id > "20"',
+          limit: '6',
+        },
+      },
+    }
+
+    createClient({
+      middlewares: [
+        (next) => (req, res) => {
+          const body = reqStubs[reqCount].body
+          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+            reqStubs[reqCount].query
+          )
+
+          reqCount += 1
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    })
+
+    return Process(request, () => Promise.resolve('OK'), { total: 46 }).then(
+      (response) => {
+        expect(response).toEqual(['OK', 'OK', 'OK'])
+      }
+    )
+  })
+
+  test('process and resolve pagination by preserving original query', () => {
+    let reqCount = 0
+    const reqStubs = {
+      0: {
+        body: createPayloadResult(5),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: 'name (en = "Foo")',
+          limit: '5',
+        },
+      },
+      1: {
+        body: createPayloadResult(2),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: ['id > "5"', 'name (en = "Foo")'],
+          limit: '5',
+        },
+      },
+    }
+
+    createClient({
+      middlewares: [
+        (next) => (req, res) => {
+          const body = reqStubs[reqCount].body
+          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+            reqStubs[reqCount].query
+          )
+
+          reqCount += 1
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    }) as any
+
+    return Process(
+      {
+        ...request,
+        uri: `${request.uri}?${qs.stringify({
+          sort: 'createdAt desc',
+          where: 'name (en = "Foo")',
+          limit: 5,
+        })}`,
+      },
+      () => Promise.resolve('OK'),
+      {}
+    )
+  })
+
+  test('process should not call fn when last page is empty', async () => {
+    let reqCount = 0
+    const reqStubs = {
+      0: {
+        body: createPayloadResult(5),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: 'name (en = "Foo")',
+          limit: '5',
+        },
+      },
+      1: {
+        body: createPayloadResult(5, 5),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: ['id > "5"', 'name (en = "Foo")'],
+          limit: '5',
+        },
+      },
+      2: {
+        body: createPayloadResult(0),
+        query: {
+          sort: ['id asc', 'createdAt desc'],
+          withTotal: 'false',
+          where: ['id > "10"', 'name (en = "Foo")'],
+          limit: '5',
+        },
+      },
+    }
+
+    createClient({
+      middlewares: [
+        (next) => (req, res) => {
+          const body = reqStubs[reqCount].body
+          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+            reqStubs[reqCount].query
+          )
+
+          reqCount += 1
+          next(req, { ...res, body, statusCode: 200 })
+        },
+      ],
+    })
+
+    let fnCall = 0
+    const processRes = await Process(
+      {
+        ...request,
+        uri: `${request.uri}?${qs.stringify({
+          sort: 'createdAt desc',
+          where: 'name (en = "Foo")',
+          limit: 5,
+        })}`,
+      },
+      (res) => {
+        expect(res.body.results).toEqual(reqStubs[fnCall].body.results)
+        expect(fnCall).toBeLessThan(2) // should not call fn if the last page is empty
+
+        fnCall += 1
+        return Promise.resolve(`OK${fnCall}`)
+      },
+      {
+        accumulate: true,
+      }
+    )
+
+    expect(processRes).toEqual(['OK1', 'OK2']) // results from fn calls
+    expect(fnCall).toBe(2) // fn was called two times
+  })
+
+  test('process and reject a request', () => {
+    createClient({
+      middlewares: [
+        (next) => (req, res) => {
+          const httpError = new Error('Invalid password')
+          const error: HttpErrorType = {
+            name: 'error',
+            message: httpError.message,
+            code: 400,
+            status: 400,
+            statusCode: 400,
+            originalRequest: req,
+          }
+          const _res: MiddlewareResponse = { ...res, error, statusCode: 400 }
+          next(req, _res)
+        },
+      ],
+    }) as any
+
+    return Process(request, () => Promise.resolve('OK'), {})
+      .then(() =>
+        Promise.reject(
+          new Error(
+            'This function should never be called, the response was rejected'
+          )
+        )
+      )
+      .catch((error) => {
+        expect(error.message).toEqual('Invalid password')
+        return Promise.resolve()
+      })
+  })
+
+  test('process and reject on rejection from user', () => {
+    createClient({
+      middlewares: [
+        (next) => (req, res) => {
+          next(req, { ...res, statusCode: 200 })
+        },
+      ],
+    }) as any
+
+    return Process(
+      request,
+      () => Promise.reject(new Error('Rejection from user')),
+      {}
+    )
+      .then(() =>
+        Promise.reject(
+          new Error(
+            'This function should never be called, the response was rejected'
+          )
+        )
+      )
+      .catch((error) => {
+        expect(error).toEqual(new Error('Rejection from user'))
+      })
+  })
+
+  test('should process and project details', async () => {
+    const projectKey = process.env.CTP_PROJECT_KEY
+    const authMiddlewareOptions = {
+      host: 'https://auth.europe-west1.gcp.commercetools.com',
+      projectKey,
+      credentials: {
+        clientId: process.env.CTP_CLIENT_ID || '',
+        clientSecret: process.env.CTP_CLIENT_SECRET || '',
+      },
+      oauthUri: process.env.adminAuthUrl || '',
+      scopes: [`manage_project:${projectKey}`],
+      fetch,
+    }
+
+    const httpMiddlewareOptions = {
+      host: 'https://api.europe-west1.gcp.commercetools.com',
+      fetch,
+    }
+
+    const apiRoot = createApiBuilderFromCtpClient(
+      new ClientBuilder()
+        .withProjectKey(projectKey)
+        .withClientCredentialsFlow(authMiddlewareOptions)
+        .withHttpMiddleware(httpMiddlewareOptions)
+        .build()
+    )
+
+    // @ts-ignore
+    const request = await apiRoot.withProjectKey({ projectKey }).get().request
+    const fn = (data) => data
+
+    Process(request, fn, {})
+      /**
+       * response is an array of processed results
+       */
+      .then((response) => {
+        // @ts-ignore
+        expect(response[0].body.key).toEqual(process.env.CTP_PROJECT_KEY)
+      })
+      .catch(fn)
   })
 })
