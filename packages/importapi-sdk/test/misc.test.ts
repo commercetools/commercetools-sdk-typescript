795c362671087f2b4ffea9b8cb663c3c42c699fe
Chukwuemeka Ajima <meeky.ae@gmail.com>
2022-07-06 14:49:53 +0200

    chore(importapi): replace importSinks method with importContainers

--- packages/importapi-sdk/test/misc.test.ts
+++ packages/importapi-sdk/test/misc.test.ts
@@ -2,7 +2,7 @@ import { ctpApiBuilder } from './helpers/ctp-api-helper'
 
 test.skip('check can get project info', async () => {
   const res = await ctpApiBuilder
-    .importSinks()
+    .importContainers()
     .get({ queryArgs: { limit: 1, offset: 0 } })
     .execute()
   expect(res.statusCode).toEqual(200)
