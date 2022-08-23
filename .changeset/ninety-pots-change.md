fee0860d44b49b9f9d1655c080bdd30b9d524859
Chukwuemeka Ajima <meeky.ae@gmail.com>
2022-08-23 12:42:27 +0200

    chore(changeset): add changeset
    - add changeset to release changes

---
+++ .changeset/ninety-pots-change.md
@@ -0,0 +1,5 @@
+---
+'@commercetools/sdk-client-v2': patch
+---
+
+Change token fetch window from 2 hours to 5 minutes
--- packages/sdk-client/src/sdk-middleware-auth/base-auth-flow.ts
+++ packages/sdk-client/src/sdk-middleware-auth/base-auth-flow.ts
@@ -28,7 +28,7 @@ function mergeAuthHeader(
 function calculateExpirationTime(expiresIn: number): number {
   return (
     Date.now() +
-    // Add a gap of 2 hours before expiration time.
+    // Add a gap of 5 minutes before expiration time.
     expiresIn * 1000 -
     5 * 60 * 1000
   )
