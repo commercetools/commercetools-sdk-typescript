5f64f4818ec32fbd38efab299d40e11f8aa3615d
Chukwuemeka Ajima <meeky.ae@gmail.com>
2022-09-13 02:29:45 +0200

    fix(http-middleware): fix requests with image/* header content-type
    - fix the if check to prevent known headers from being overriden to 'application/json'

--- packages/sdk-client/src/sdk-middleware-http/http.ts
+++ packages/sdk-client/src/sdk-middleware-http/http.ts
@@ -119,9 +119,12 @@ export default function createHttpMiddleware({
       if (requestHeader['Content-Type'] === null) {
         delete requestHeader['Content-Type']
       }
+
       if (
-        !Object.prototype.hasOwnProperty.call(requestHeader, 'Content-Type') ||
-        !Object.prototype.hasOwnProperty.call(requestHeader, 'content-type')
+        !(
+          Object.prototype.hasOwnProperty.call(requestHeader, 'Content-Type') ||
+          Object.prototype.hasOwnProperty.call(requestHeader, 'content-type')
+        )
       ) {
         requestHeader['Content-Type'] = 'application/json'
       }
