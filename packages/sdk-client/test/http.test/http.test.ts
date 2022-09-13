be26c02a4ce0cde9affd97cd5acb508710273462
Barbara Palumbo <barbara.palumbo@commercetools.de>
2022-09-13 18:26:59 +0200

    chore(http): fix test and code after updated of nodejs

--- packages/sdk-client/test/http.test/http.test.ts
+++ packages/sdk-client/test/http.test/http.test.ts
@@ -1,9 +1,9 @@
 import nock from 'nock'
-import fetch from 'node-fetch'
 import AbortController from 'abort-controller'
 import { createHttpMiddleware } from '../../src/sdk-middleware-http'
 import { MiddlewareRequest, MiddlewareResponse } from '../../src/types/sdk.d'
 
+const fetch = require('node-fetch')
 const Buffer = require('buffer/').Buffer
 function createTestRequest(options) {
   return {
