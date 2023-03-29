51446ea3c27cb714603e47c3bf1981def78597fd
Auto Mation <automation@commercetools.com>
2023-03-29 11:50:08 +0000

    build(codegen): updating SDK

--- packages/platform-sdk/src/generated/models/common.ts
+++ packages/platform-sdk/src/generated/models/common.ts
@@ -540,12 +540,12 @@ export type _BaseResource =
   | TaxCategory
   | Type
   | Zone
+  | BusinessUnit
+  | Channel
   | AttributeGroup
-  | CartDiscount
   | Cart
   | Category
-  | Channel
-  | BusinessUnit
+  | CartDiscount
 /**
  *	These objects represent information about which [API Client](/../api/projects/api-clients) created or modified a resource. For more information, see [Client Logging](/client-logging).
  *
