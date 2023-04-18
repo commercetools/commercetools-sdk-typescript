673154461e7a58e19aac4abbd5a71561d57cdacc
Chukwuemeka Ajima <meeky.ae@gmail.com>
2023-04-18 15:28:44 +0100

    chore(querystring): merge remote

--- packages/platform-sdk/test/request-utils.spec.ts
+++ packages/platform-sdk/test/request-utils.spec.ts
@@ -33,8 +33,8 @@ describe('ApiRequestExecutor', () => {
     })
 
     test('handle invalid characters', async () => {
-      // expect(testQuery({ foo: '<>abc /' })).toEqual('?foo=%3C%3Eabc+%2F')
       expect(testQuery({ foo: '<>abc /' })).toEqual('?foo=%3C%3Eabc%20%2F')
+      // expect(testQuery({ foo: '<>abc /' })).toEqual('?foo=%3C%3Eabc+%2F')
     })
 
     test('handle empty arrays', async () => {
