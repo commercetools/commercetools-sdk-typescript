5f311659ef61b14e3a3421ac65aac2bb0f606a3f
Barbara Palumbo <barbara.palumbo@commercetools.de>
2023-05-31 18:56:28 +0200

    test: reset changes on jest config

--- jest.config.js
+++ jest.config.js
@@ -21,8 +21,4 @@ module.exports = {
         ]
       : null,
   ].filter(Boolean),
-  testMatch: [
-    '**/__tests__/**/*.ts?(x)', // Default pattern for unit tests
-    '**/?(*.)+(spec|integration.spec).ts?(x)', // Pattern for integration tests
-  ],
 }
