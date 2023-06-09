90852d362a3d175c614b87d2b2422f1634f98dba
Jens Schulze <jens.schulze@commercetools.com>
2023-06-09 10:05:20 +0200

    test: update env variables

--- packages/platform-sdk/test/integration-tests/test-utils.ts
+++ packages/platform-sdk/test/integration-tests/test-utils.ts
@@ -2,7 +2,7 @@ import { ClientBuilder } from '@commercetools/sdk-client-v2'
 import { createApiBuilderFromCtpClient } from '../../src'
 const fetch = require('node-fetch')
 
-export const projectKey = process.env.IT_CTP_PROJECT_KEY
+export const projectKey = process.env.CTP_PROJECT_KEY
 const httpMiddlewareOptions = {
   host:
     process.env.CTP_API_URL || 'https://api.europe-west1.gcp.commercetools.com',
@@ -14,8 +14,8 @@ const authMiddlewareOptions = {
     'https://auth.europe-west1.gcp.commercetools.com',
   projectKey,
   credentials: {
-    clientId: process.env.IT_CTP_CLIENT_ID,
-    clientSecret: process.env.IT_CTP_CLIENT_SECRET,
+    clientId: process.env.CTP_CLIENT_ID,
+    clientSecret: process.env.CTP_CLIENT_SECRET,
   },
   fetch,
 }
