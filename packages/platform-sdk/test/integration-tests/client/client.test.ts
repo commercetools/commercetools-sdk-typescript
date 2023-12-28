aed579971bc8cf1dba22c7b8f0baa0f24722818d
lojzatran <lam.tran@commercetools.com>
2023-12-28 18:04:00 +0000

    test(integration): fix test for anonymous session

--- packages/platform-sdk/test/integration-tests/client/client.test.ts
+++ packages/platform-sdk/test/integration-tests/client/client.test.ts
@@ -1,28 +1,50 @@
 import { requireEnvVar } from '../../helpers/test-utils'
-import { ClientBuilder } from '@commercetools/sdk-client-v2/src'
+import { ClientBuilder, ClientRequest } from '@commercetools/sdk-client-v2/src'
 
-export const projectKey = requireEnvVar('CTP_PROJECT_KEY')
+const projectKey = requireEnvVar('CTP_PROJECT_KEY')
 const clientId = requireEnvVar('CTP_CLIENT_ID')
 const clientSecret = requireEnvVar('CTP_CLIENT_SECRET')
 const authURL = requireEnvVar('CTP_AUTH_URL')
 
 describe('testing api root client', () => {
-  it.skip('should create api root client with anonymous session', async () => {
-    const ctpClient = new ClientBuilder()
-      .withAnonymousSessionFlow({
-        host: authURL,
-        projectKey,
-        credentials: {
-          clientId: clientId,
-          clientSecret: clientSecret,
-        },
-      })
+  it('should create api root client with anonymous session', async () => {
+    const authMiddlewareOptions = {
+      host: authURL,
+      projectKey,
+      credentials: {
+        clientId: clientId,
+        clientSecret: clientSecret,
+      },
+      scopes: [`manage_project:${projectKey}`],
+      fetch,
+    }
+
+    const httpMiddlewareOptions = {
+      host: authURL,
+      headersWithStringBody: ['application/x-www-form-urlencoded'],
+      fetch,
+    }
+
+    const client = new ClientBuilder()
+      .withHttpMiddleware(httpMiddlewareOptions)
       .build()
 
-    const response = await ctpClient.execute({
+    const request: ClientRequest = {
       method: 'POST',
-      uri: `https://auth.europe-west1.gcp.commercetools.com/oauth/${projectKey}/anonymous/token`,
-    })
+      uri: `/oauth/${projectKey}/anonymous/token`,
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${authMiddlewareOptions.credentials.clientId}:${authMiddlewareOptions.credentials.clientSecret}`
+        ).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=client_credentials&scope=${authMiddlewareOptions.scopes.join(
+        ' '
+      )}`,
+    }
+
+    const response = await client.execute(request)
     expect(response.statusCode).toBe(200)
+    expect(response.body.access_token).toBeDefined()
   })
 })
