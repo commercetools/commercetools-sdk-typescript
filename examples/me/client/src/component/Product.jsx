7444f4a99e2719c0a38cc28831072c9dd00c9e34
lojzatran <lam.tran@commercetools.com>
2024-03-20 21:47:48 +0100

    fix(examples): update outdated dependencies

--- examples/me/client/src/component/Product.jsx
+++ examples/me/client/src/component/Product.jsx
@@ -8,9 +8,9 @@ const incart = false
 
 const Product = ({ product, handler }) => {
   const name = product?.masterData?.current?.name?.en
-  const image = product?.masterData?.current?.masterVariant?.images[0].url
-  const currencyCode = product?.masterData?.current?.masterVariant?.prices[0].value.currencyCode
-  const centAmount = product?.masterData?.current?.masterVariant?.prices[0].value.centAmount
+  const image = product?.masterData?.current?.masterVariant?.images[0]?.url
+  const currencyCode = product?.masterData?.current?.masterVariant?.prices[0]?.value.currencyCode
+  const centAmount = product?.masterData?.current?.masterVariant?.prices[0]?.value.centAmount
   return (
     <div className="col-2 mx-auto col-md-6 col-lg-3" style={{ marginBottom: '25px' }}>
       <Card onClick={() => { }} style={{ width: '18rem' }}>
