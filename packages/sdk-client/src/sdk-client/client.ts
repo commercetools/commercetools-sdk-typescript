commercetools/commercetools-sdk-typescript

commit a83e653c986e50f31f71546edc721263d29990b5

Remove all `querystring` dependency (#453)
* chore(querystring): remove querystring dependency

- remove deprecated querystring dependency
- add qs dependency to package
- refactor code to accommodate new implementation

* Create eleven-meals-whisper.md

* chore(querystring-and-qs): querystring and qs dependencies

- remove all querystring and qs dependencies and reference
- use native URLSearchParams instead
- include util function for stringifying and parsing url/search parameters

* chore(url-search-params): add custom encoding function

- add functions to encode and decode url strings

* chore(querystring): merge remote

* Update packages/sdk-client/src/utils/url.ts

Co-authored-by: Jens Schulze <jens.schulze@commercetools.com>

---------

Co-authored-by: Jens Schulze <jens.schulze@commercetools.com>

packages/sdk-client/src/sdk-client/client.ts
@@ -1,4 +1,3 @@
-import qs from 'querystring'
 import {
   Client,
   ClientOptions,
@@ -12,6 +11,7 @@ import {
   SuccessResult,
 } from '../types/sdk.d'
 import validate from './validate'
+import { stringifyURLString, parseURLString } from '../utils'
 
 let _options
 function compose(...funcs: Array<Function>): Function {
@@ -53,7 +53,9 @@ export function process(
       _path = path
       _queryString = queryString
     }
-    const requestQuery = { ...qs.parse(_queryString) }
+    // const requestQuery = { ...qs.parse(_queryString) }
+    // const requestQuery = { ...Object.fromEntries(new URLSearchParams(_queryString)) }
+    const requestQuery = { ...parseURLString<object>(_queryString) }
     const query = {
       // defaults
       limit: 20,
@@ -66,14 +68,21 @@ export function process(
     const processPage = async (lastId?: string, acc: Array<any> = []) => {
       // Use the lesser value between limit and itemsToGet in query
       const limit = query.limit < itemsToGet ? query.limit : itemsToGet
-      const originalQueryString = qs.stringify({ ...query, limit })
+      // const originalQueryString = qs.stringify({ ...query, limit }, qsOptions)
+      const originalQueryString = new URLSearchParams({
+        ...query,
+        limit,
+      } as unknown as Record<string, string>).toString()
 
       const enhancedQuery = {
         sort: 'id asc',
         withTotal: false,
         ...(lastId ? { where: `id > "${lastId}"` } : {}),
       }
-      const enhancedQueryString = qs.stringify(enhancedQuery)
+      // const enhancedQueryString = qs.stringify(enhancedQuery, qsOptions)
+      const enhancedQueryString = new URLSearchParams(
+        enhancedQuery as unknown as Record<string, string>
+      ).toString()
       const enhancedRequest = {
         ...request,
         uri: `${_path}?${enhancedQueryString}&${originalQueryString}`,

packages/sdk-client/src/utils/url.ts
@@ -1,0 +1,47 @@
+function urlParser<T>(url: string | URLSearchParams): T {
+  const object = {} as T
+  const data = new URLSearchParams(url)
+  for (let x of data.keys()) {
+    if (data.getAll(x).length > 1) {
+      object[x] = data.getAll(x)
+    } else {
+      object[x] = data.get(x)
+    }
+  }
+
+  return object
+}
+
+function urlStringifier(
+  object: string | Record<string, any> | Array<Array<string>> | URLSearchParams
+): string {
+  const params = new URLSearchParams(object)
+  for (const [key, value] of Object.entries(object)) {
+    if (Array.isArray(value)) {
+      params.delete(key)
+      value.filter(Boolean).forEach((v) => params.append(key, v))
+    }
+  }
+
+  return params.toString()
+}
+
+export function parseURLString<T>(
+  url: string | URLSearchParams,
+  parser: <T>(url: string | URLSearchParams) => T = urlParser
+): T {
+  return parser<T>(url)
+}
+
+export function stringifyURLString(
+  object: string | Record<string, any> | Array<Array<string>>,
+  stringifier: (
+    object:
+      | string
+      | Record<string, any>
+      | Array<Array<string>>
+      | URLSearchParams
+  ) => string = urlStringifier
+): string {
+  return urlStringifier(object)
+}

packages/platform-sdk/test/request-utils.spec.ts
@@ -34,6 +34,7 @@ describe('ApiRequestExecutor', () => {
 
     test('handle invalid characters', async () => {
       expect(testQuery({ foo: '<>abc /' })).toEqual('?foo=%3C%3Eabc%20%2F')
+      // expect(testQuery({ foo: '<>abc /' })).toEqual('?foo=%3C%3Eabc+%2F')
     })
 
     test('handle empty arrays', async () => {

packages/sdk-client/src/utils/index.ts
@@ -1,0 +1,1 @@
+export { stringifyURLString, parseURLString } from './url'

packages/sdk-client/test/client.test/sdk-client.test.ts
@@ -1,4 +1,3 @@
-import qs from 'querystring'
 import { Process, createClient, ClientBuilder } from '../../src'
 import { createApiBuilderFromCtpClient } from '../../../platform-sdk'
 import {
@@ -7,6 +6,7 @@ import {
   MiddlewareResponse,
 } from '../../src/types/sdk.d'
 import fetch from 'node-fetch'
+import { stringifyURLString, parseURLString } from '../../src/utils'
 
 const createPayloadResult = (tot, startingId = 0) => ({
   count: tot,
@@ -278,7 +278,7 @@ describe('process', () => {
       middlewares: [
         (next) => (req, res) => {
           const body = reqStubs[reqCount].body
-          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+          expect(parseURLString(req.uri.split('?')[1])).toEqual(
             reqStubs[reqCount].query
           )
 
@@ -330,7 +330,7 @@ describe('process', () => {
       middlewares: [
         (next) => (req, res) => {
           const body = reqStubs[reqCount].body
-          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+          expect(parseURLString(req.uri.split('?')[1])).toEqual(
             reqStubs[reqCount].query
           )
 
@@ -373,8 +373,9 @@ describe('process', () => {
     const client = createClient({
       middlewares: [
         (next) => (req, res) => {
+          // console.log(req.uri.split('?')[1], '>>>>', reqStubs[reqCount].query)
           const body = reqStubs[reqCount].body
-          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+          expect(parseURLString(req.uri.split('?')[1])).toEqual(
             reqStubs[reqCount].query
           )
 
@@ -386,11 +387,11 @@ describe('process', () => {
     return client.process(
       {
         ...request,
-        uri: `${request.uri}?${qs.stringify({
+        uri: `${request.uri}?${stringifyURLString({
           sort: 'createdAt desc',
           where: 'name (en = "Foo")',
           limit: 5,
-        })}`,
+        }).toString()}`,
       },
       () => Promise.resolve('OK')
     )
@@ -432,7 +433,7 @@ describe('process', () => {
       middlewares: [
         (next) => (req, res) => {
           const body = reqStubs[reqCount].body
-          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+          expect(parseURLString(req.uri.split('?')[1])).toEqual(
             reqStubs[reqCount].query
           )
 
@@ -446,7 +447,7 @@ describe('process', () => {
     const processRes = await client.process(
       {
         ...request,
-        uri: `${request.uri}?${qs.stringify({
+        uri: `${request.uri}?${stringifyURLString({
           sort: 'createdAt desc',
           where: 'name (en = "Foo")',
           limit: 5,
@@ -601,7 +602,7 @@ describe('process - exposed', () => {
       middlewares: [
         (next) => (req, res) => {
           const body = reqStubs[reqCount].body
-          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+          expect(parseURLString(req.uri.split('?')[1])).toEqual(
             reqStubs[reqCount].query
           )
 
@@ -653,7 +654,7 @@ describe('process - exposed', () => {
       middlewares: [
         (next) => (req, res) => {
           const body = reqStubs[reqCount].body
-          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+          expect(parseURLString(req.uri.split('?')[1])).toEqual(
             reqStubs[reqCount].query
           )
 
@@ -697,7 +698,7 @@ describe('process - exposed', () => {
       middlewares: [
         (next) => (req, res) => {
           const body = reqStubs[reqCount].body
-          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+          expect(parseURLString(req.uri.split('?')[1])).toEqual(
             reqStubs[reqCount].query
           )
 
@@ -710,11 +711,11 @@ describe('process - exposed', () => {
     return Process(
       {
         ...request,
-        uri: `${request.uri}?${qs.stringify({
+        uri: `${request.uri}?${stringifyURLString({
           sort: 'createdAt desc',
           where: 'name (en = "Foo")',
           limit: 5,
-        })}`,
+        }).toString()}`,
       },
       () => Promise.resolve('OK'),
       {}
@@ -757,7 +758,7 @@ describe('process - exposed', () => {
       middlewares: [
         (next) => (req, res) => {
           const body = reqStubs[reqCount].body
-          expect(qs.parse(req.uri.split('?')[1])).toEqual(
+          expect(parseURLString(req.uri.split('?')[1])).toEqual(
             reqStubs[reqCount].query
           )
 
@@ -771,7 +772,7 @@ describe('process - exposed', () => {
     const processRes = await Process(
       {
         ...request,
-        uri: `${request.uri}?${qs.stringify({
+        uri: `${request.uri}?${stringifyURLString({
           sort: 'createdAt desc',
           where: 'name (en = "Foo")',
           limit: 5,

commit 0cbe623f179353ab950e23273fe1837c784681dd

fix(url-encoding): fix malformed uri error
- fix the bug introduced by #453 PR
- use the proper javascript serialization function
- add tests to validate functionality and the fix

packages/sdk-client/src/sdk-client/client.ts
@@ -10,7 +10,7 @@ import {
   ProcessOptions,
   SuccessResult,
 } from '../types/sdk.d'
-import { parseURLString } from '../utils'
+import { stringifyURLString, parseURLString } from '../utils'
 import validate from './validate'
 
 let _options: ClientOptions
@@ -83,20 +83,16 @@ export function process<T = any>(
       // Use the lesser value between limit and itemsToGet in query
       const limit = query.limit < itemsToGet ? query.limit : itemsToGet
       // const originalQueryString = qs.stringify({ ...query, limit }, qsOptions)
-      const originalQueryString = new URLSearchParams({
-        ...query,
-        limit,
-      } as unknown as Record<string, string>).toString()
+      const originalQueryString = stringifyURLString({ ...query, limit })
 
       const enhancedQuery = {
         sort: opt.sort || 'id asc',
         withTotal: false,
         ...(lastId ? { where: `id > "${lastId}"` } : {}),
       }
+
       // const enhancedQueryString = qs.stringify(enhancedQuery, qsOptions)
-      const enhancedQueryString = new URLSearchParams(
-        enhancedQuery as unknown as Record<string, string>
-      ).toString()
+      const enhancedQueryString = stringifyURLString(enhancedQuery)
       const enhancedRequest = {
         ...request,
         uri: `${_path}?${enhancedQueryString}&${originalQueryString}`,

packages/sdk-client/src/utils/url.ts
@@ -43,5 +43,5 @@ export function stringifyURLString(
       | URLSearchParams
   ) => string = urlStringifier
 ): string {
-  return urlStringifier(object)
+  return stringifier(object)
 }

packages/platform-sdk/test/integration-tests/process/process.test.ts
@@ -3,9 +3,9 @@ import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk'
 import fetch from 'node-fetch'
 
 describe('integration test for process function', () => {
-  test('should process customer list using provided limit', async () => {
-    const projectKey = process.env.CTP_PROJECT_KEY
-    const authMiddlewareOptions = {
+  const projectKey = process.env.CTP_PROJECT_KEY
+  function getAuthOptions(options: object = {}) {
+    return {
       host: 'https://auth.europe-west1.gcp.commercetools.com',
       projectKey,
       credentials: {
@@ -15,17 +15,23 @@ describe('integration test for process function', () => {
       oauthUri: process.env.adminAuthUrl || '',
       scopes: [`manage_project:${projectKey}`],
       fetch,
+      ...options,
     }
+  }
 
-    const httpMiddlewareOptions = {
+  function getHttpOptions(options: object = {}) {
+    return {
       host: 'https://api.europe-west1.gcp.commercetools.com',
       fetch,
+      ...options,
     }
+  }
 
+  test('should process customer list using provided limit', async () => {
     const client = new ClientBuilder()
       .withProjectKey(projectKey)
-      .withClientCredentialsFlow(authMiddlewareOptions)
-      .withHttpMiddleware(httpMiddlewareOptions)
+      .withClientCredentialsFlow(getAuthOptions())
+      .withHttpMiddleware(getHttpOptions())
       .build()
 
     const apiRoot = createApiBuilderFromCtpClient(client)
@@ -54,27 +60,10 @@ describe('integration test for process function', () => {
   })
 
   test('should process customer list using provided limit', async () => {
-    const projectKey = process.env.CTP_PROJECT_KEY
-    const authMiddlewareOptions = {
-      host: 'https://auth.europe-west1.gcp.commercetools.com',
-      projectKey,
-      credentials: {
-        clientId: process.env.CTP_CLIENT_ID || '',
-        clientSecret: process.env.CTP_CLIENT_SECRET || '',
-      },
-      scopes: [`manage_project:${projectKey}`],
-      fetch,
-    }
-
-    const httpMiddlewareOptions = {
-      host: 'https://api.europe-west1.gcp.commercetools.com',
-      fetch,
-    }
-
     const client = new ClientBuilder()
       .withProjectKey(projectKey)
-      .withClientCredentialsFlow(authMiddlewareOptions)
-      .withHttpMiddleware(httpMiddlewareOptions)
+      .withClientCredentialsFlow(getAuthOptions())
+      .withHttpMiddleware(getHttpOptions())
       .build()
 
     const apiRoot = createApiBuilderFromCtpClient(client)
@@ -103,27 +92,10 @@ describe('integration test for process function', () => {
   })
 
   test('should process customer list using provided limit', () => {
-    const projectKey = process.env.CTP_PROJECT_KEY
-    const authMiddlewareOptions = {
-      host: 'https://auth.europe-west1.gcp.commercetools.com',
-      projectKey,
-      credentials: {
-        clientId: process.env.CTP_CLIENT_ID || '',
-        clientSecret: process.env.CTP_CLIENT_SECRET || '',
-      },
-      scopes: [`manage_project:${projectKey}`],
-      fetch,
-    }
-
-    const httpMiddlewareOptions = {
-      host: 'https://api.europe-west1.gcp.commercetools.com',
-      fetch,
-    }
-
     const client = new ClientBuilder()
       .withProjectKey(projectKey)
-      .withClientCredentialsFlow(authMiddlewareOptions)
-      .withHttpMiddleware(httpMiddlewareOptions)
+      .withClientCredentialsFlow(getAuthOptions())
+      .withHttpMiddleware(getHttpOptions())
       .build()
 
     const apiRoot = createApiBuilderFromCtpClient(client)
@@ -147,6 +119,36 @@ describe('integration test for process function', () => {
         const id2 = response[0].body.results[1].id
 
         expect(id1 > id2).toBe(true)
+      })
+      .catch(fn)
+  })
+
+  test('should properly the `expan` args as a uri parameter', () => {
+    const client = new ClientBuilder()
+      .withProjectKey(projectKey)
+      .withClientCredentialsFlow(getAuthOptions())
+      .withHttpMiddleware(getHttpOptions())
+      .build()
+
+    const apiRoot = createApiBuilderFromCtpClient(client)
+
+    const request = apiRoot
+      .withProjectKey({ projectKey })
+      .categories()
+      .get({
+        queryArgs: {
+          limit: 2,
+          withTotal: false,
+          expand: ['parent', 'ancestors[*]'],
+        },
+      })
+      .clientRequest()
+
+    const fn = (data: any) => data
+    return client
+      .process(request, fn, {})
+      .then((response) => {
+        expect(response[0].statusCode).toEqual(200)
       })
       .catch(fn)
   })