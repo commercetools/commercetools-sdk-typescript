77c5abbd47d832669ad199d26459a587666c0129
Chukwuemeka Ajima <meeky.ae@gmail.com>
2024-07-06 04:46:05 +0200

    chore(browser.html): fix issues browser example
    - fix issues with browser example
    - replace webpack module bundle in favour of esbuild
    - minor refactor

---
+++ esbuild.cjs
@@ -0,0 +1,32 @@
+const esbuild = require('esbuild');
+const { exec } = require('child_process');
+
+const generalConfig = { minify: true, bundle: true, write: true }
+
+const sdkClientConfig = esbuild.build(
+  Object.assign({}, generalConfig, {
+    entryPoints: ['packages/platform-sdk/src/index.ts'],
+    globalName: 'window["@commercetools/platform-sdk"]',
+    outfile: 'packages/platform-sdk/dist/commercetools-platform-sdk.umd.js',
+  })
+)
+
+const sdkClientV3Config = esbuild.build(
+  Object.assign({}, generalConfig, {
+    entryPoints: ['packages/sdk-client-v3/src/index.ts'],
+    globalName: 'window["@commercetools/ts-client"]',
+    outfile: 'packages/sdk-client-v3/dist/commercetools-ts-client.umd.js'
+  })
+)
+
+const platformSdkConfig = esbuild.build(
+  Object.assign({}, generalConfig, {
+    entryPoints: ['packages/sdk-client/src/index.ts'],
+    globalName: 'window["@commercetools/sdk-client-v2"]',
+    outfile: 'packages/sdk-client/dist/commercetools-sdk-client-v2.umd.js'
+  })
+)
+
+Promise.all([
+  sdkClientConfig, sdkClientV3Config, platformSdkConfig
+]).catch(console.error);
