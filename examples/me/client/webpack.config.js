2f25a6efb3f9093befdcb7dd2974645abfae03cc
Chukwuemeka Ajima <meeky.ae@gmail.com>
2025-01-17 14:13:25 +0100

    chore(me-demo-app): fix issue with webpack
    - fix issues with webpack v5 breaking change

--- examples/me/client/webpack.config.js
+++ examples/me/client/webpack.config.js
@@ -59,6 +59,12 @@ const generalConfig = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
     ],
   },
   resolve: {
