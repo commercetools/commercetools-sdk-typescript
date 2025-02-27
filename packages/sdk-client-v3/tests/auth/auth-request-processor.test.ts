f762b6e4928ae8eeafdeb1ad70b42250719a8eda
Chukwuemeka Ajima <meeky.ae@gmail.com>
2025-02-27 03:35:47 +0100

    chore(tests): add more tests
    - add more tests to improve coverage

---
+++ packages/sdk-client-v3/tests/auth/auth-request-processor.test.ts
@@ -0,0 +1,64 @@
+import {
+  MiddlewareRequest,
+  MiddlewareResponse,
+  TokenCacheOptions,
+  TokenStore,
+} from '../../src'
+import { authProcessor } from '../../src/middleware/auth-middleware/auth-request-processor'
+
+function createTestRequest(options) {
+  return {
+    url: '',
+    method: 'POST',
+    body: null,
+    headers: {},
+    ...options,
+  }
+}
+
+describe('auth-request-processor', () => {
+  test('should pause while token is being fetched', async () => {
+    const request = createTestRequest({
+      resolve: Promise.resolve,
+      reject: Promise.reject,
+    })
+
+    const next = async (req: MiddlewareRequest) => {
+      console.log(req, '>>>>>-->')
+      expect(req.headers.Authorization).toEqual('Bearer token')
+      return req as unknown as Promise<MiddlewareResponse<unknown>>
+    }
+
+    await authProcessor(
+      request,
+      Promise.resolve(true),
+      {
+        token: '',
+        expirationTime: 0,
+      },
+      {
+        clientId: '',
+        projectKey: '',
+        host: '',
+      },
+      {
+        set: () => {},
+        get: () => ({
+          token: 'token',
+          expirationTime: 99999,
+        }),
+      },
+      (o) => ({ url: '', basicAuth: '', body: '' }),
+      {
+        host: '',
+        projectKey: '',
+        credentials: {
+          clientId: '',
+          clientSecret: '',
+          anonymousId: '',
+        },
+      },
+      next
+    )
+  })
+})
