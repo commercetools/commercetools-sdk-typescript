9b88d8bde989db1b80f972e159406810a3b39c0f
Chukwuemeka Ajima <meeky.ae@gmail.com>
2025-03-26 16:09:27 +0100

    fix(ts-sdk-apm): fix issue with telemetry package
    - fix issue with telemetry tracer and apm functions not being called
    - fix tests and add more tests to ensure functions are being called

--- packages/ts-sdk-apm/test/apm.test/apm.test.ts
+++ packages/ts-sdk-apm/test/apm.test/apm.test.ts
@@ -1,4 +1,3 @@
-import { ClientBuilder, Next } from '@commercetools/ts-client'
 import { type MiddlewareRequest, createTelemetryMiddleware } from '../../src'
 import { recordDatadog } from '../../src/helpers/datadogHelper'
 import datadog from 'dd-trace'
@@ -65,15 +64,15 @@ describe('apm', () => {
   })
 
   describe('apm test - non-null tracer configuration', () => {
-    const options = {
-      apm: jest.fn(() => ({ a: 'apm-module' })),
-      tracer: jest.fn(() => ({ t: 'tracer-module' })),
-    }
-
-    const response = createTestResponse({})
-    const telemetryMiddleware = createTelemetryMiddleware(options)
-
     test('adds an `apm` and `tracer` properties in request object', () => {
+      const options = {
+        apm: jest.fn(() => ({ a: 'apm-module' })),
+        tracer: jest.fn(() => ({ t: 'tracer-module' })),
+      }
+
+      const response = createTestResponse({})
+      const telemetryMiddleware = createTelemetryMiddleware(options)
+
       const next = async (req: MiddlewareRequest) => {
         expect(req['apm']).toBeTruthy()
         expect(req['tracer']).toBeTruthy()
@@ -88,12 +87,46 @@ describe('apm', () => {
         expect(req['tracer']).toHaveReturned()
 
         expect(options.apm).toHaveBeenCalled()
-        expect(options.tracer).toHaveBeenCalled
+        expect(options.tracer).toHaveBeenCalled()
 
         return response
       }
 
       telemetryMiddleware(next)(request)
+    })
+
+    test('should ensure `apm` function is being called', async () => {
+      const opts = {
+        apm: jest.fn(),
+      }
+
+      const response = createTestResponse({})
+      const telemetryMiddleware = createTelemetryMiddleware(opts)
+
+      const next = async (req: MiddlewareRequest) => {
+        expect(req['apm']).toBeDefined()
+        expect(opts.apm).toHaveBeenCalled()
+        return response
+      }
+
+      await telemetryMiddleware(next)({ method: 'POST' })
+    })
+
+    test('should ensure `tracer` function is being called', async () => {
+      const opts = {
+        tracer: jest.fn(),
+      }
+
+      const response = createTestResponse({})
+      const telemetryMiddleware = createTelemetryMiddleware(opts)
+
+      const next = async (req: MiddlewareRequest) => {
+        expect(req['tracer']).toBeDefined()
+        expect(opts.tracer).toHaveBeenCalled()
+        return response
+      }
+
+      await telemetryMiddleware(next)({ method: 'POST' })
     })
   })
 
