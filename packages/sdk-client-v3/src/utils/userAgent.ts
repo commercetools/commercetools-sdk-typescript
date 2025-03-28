9bf06f780f885d4e2693d6535b7d3658df2761d1
Chukwuemeka Ajima <meeky.ae@gmail.com>
2025-03-28 14:32:16 +0100

    fix(user-agent): fix issue with unknown envs
    - fix issue process object in an unknown enviornment
    - assert that the process object exists and contains the necessary properties

--- packages/sdk-client-v3/src/utils/userAgent.ts
+++ packages/sdk-client-v3/src/utils/userAgent.ts
@@ -16,8 +16,8 @@ const isBrowser = (): boolean =>
 function getSystemInfo(): string {
   if (isBrowser()) return window.navigator.userAgent
 
-  const nodeVersion: string = process?.version.slice(1) || 'unknow' // unknow environment like  React Native etc
-  const platformInfo = `(${process.platform}; ${process.arch})`
+  const nodeVersion: string = process?.version?.slice(1) || 'unknow' // unknow environment like  React Native etc
+  const platformInfo = `(${process?.platform}; ${process?.arch})`
 
   return `node.js/${nodeVersion} ${platformInfo}`
 }
