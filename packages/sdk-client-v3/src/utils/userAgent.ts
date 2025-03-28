8d76fb7d57350395b37174980ba7c548971d82a1
Chukwuemeka Ajima <meeky.ae@gmail.com>
2025-03-28 14:48:38 +0100

    fix(user-agent): fix issue with unknown envs
    - minor fix

--- packages/sdk-client-v3/src/utils/userAgent.ts
+++ packages/sdk-client-v3/src/utils/userAgent.ts
@@ -17,7 +17,7 @@ function getSystemInfo(): string {
   if (isBrowser()) return window.navigator.userAgent
 
   const nodeVersion: string = process?.version?.slice(1) || 'unknow' // unknow environment like  React Native etc
-  const platformInfo = `(${process?.platform}; ${process?.arch})`
+  const platformInfo = `(${process?.platform || ''}; ${process?.arch || ''})`
 
   return `node.js/${nodeVersion} ${platformInfo}`
 }
